9ce377e4716f9d499c483e10dcc00eaff204b4ad
Andrey Morkovkin <mork.andrey.s.dev@gmail.com>
2022-05-08 22:24:18 +0300

    Refactor tools

--- tools/plot_images.py
+++ tools/plot_images.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import os
+import argparse
 
 def plotGeneratedImages(directory, result, dim=(3, 3), figsize=(3, 3)):
     #noise = np.random.normal(0, 1, size=[examples, randomDim])
@@ -8,16 +9,20 @@ def plotGeneratedImages(directory, result, dim=(3, 3), figsize=(3, 3)):
     for address, dirs, files in os.walk(directory):
         for file in files:
             if file.endswith('png'):
-                images.append(plt.imread(os.path.join(address, file)))
+                images.append((file, plt.imread(os.path.join(address, file))))
     plt.figure(figsize=figsize)
     for i in range(9):
         plt.subplot(dim[0], dim[1], i+1)
-        plt.imshow(images[i], interpolation='nearest', cmap='gray_r')
+        plt.imshow(images[i][0], interpolation='nearest', cmap='gray_r')
         plt.axis('off')
     plt.tight_layout()
     plt.savefig(f'{result}.png')
 
-for number in [10, 25, 50, 75, 100, 125, 150, 175]:
-    for postfix in ['', '_grad_cam_real', '_grad_cam_fake']:
-        string = f'generated_epoch_{number}{postfix}'
-        plotGeneratedImages(f'/home/masdevas/Documents/Projects/_convnet_2_v1/24_11_2021-01_24_19/{string}', string)
+def main():
+    parser = argparse.ArgumentParser(description='Tool for generation grid of images.')
+    parser.add_argument('images', type=str, help='Input dir for images.')
+    args = parser.parse_args()
+    plotGeneratedImages(args.images, os.path.join(args.images, 'grid'))
+
+if __name__ == '__main__':
+    main()
